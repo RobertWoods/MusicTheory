37db5724f0237b9b1ced9e661fd20d13772d95a9
Robert Woods <robertwoods2013@gmail.com>
2017-05-12 15:48:04 -0400

    added minor key support

--- models/key.py
+++ models/key.py
@@ -1,12 +1,16 @@
 class Key:
     def __init__(self, signature, minor=0):
-        if minor == 0:      #major
-            self.signature = signature
-        elif minor == 1: #minor
-            pass
+        self.signature = signature
+        self.descending = None
+        if minor == 1: #minor
+            self.signature[5] -= 1
+            self.signature[6] -= 1
         elif minor == 2: #harmonic minor
-            pass
+            self.signature[5] -= 1
         elif minor == 3: #melodic minor
+            self.descending = list(self.signature)
+            self.descending[5] -= 1
+            self.descending[6] -= 1
             pass
 
     def has_note(self, note, circle):
@@ -24,3 +28,6 @@ class Key:
 
     def __delitem__(self, key):
         del self.signature[key]
+
+    def is_melodic(self):
+        return self.descending is not None
